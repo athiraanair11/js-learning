662147507292a352fc1a991750172d45ae8ffa9c
athiraanil11 <athiraanil093@gmail.com>
2022-05-28 11:47:33 +0530

    learned

---
+++ large.js
@@ -0,0 +1,9 @@
+let x=74
+let y=27
+if(x>y)
+{
+    console.log(x)
+}
+else{
+    console.log(y)
+}
---
+++ small.js
@@ -0,0 +1,10 @@
+let x=43
+let y=58
+if(x<y)
+{
+    console.log(x)
+}
+else
+{
+    console.log(y)
+}
